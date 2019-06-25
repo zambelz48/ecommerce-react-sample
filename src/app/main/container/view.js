d87260cda801647a63c528632add9a78bf39ec76
Nanda . J . A <nanda.julianda48@gmail.com>
2019-06-26 04:25:51 +0700

    Update main app container

--- src/app/main/container/view.js
+++ src/app/main/container/view.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import ProductContainer from 'app/product/container'
+import CartContainer from 'app/cart/container'
+import 'app/main/styles/main-app.scss'
 
 export default class AppView extends Component {
   render() {
     return (
-      <div className='main-layout'>
-        Initial setup
+      <div className='container-fluid main-container'>
+        <ProductContainer>
+        </ProductContainer>
+        <div className="main-border"></div>
+        <CartContainer>
+        </CartContainer>
       </div>
     )
   }
